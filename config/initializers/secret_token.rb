f4815aac551c18ee3a7d8fdf132baa7a17ec34bd
SailorMercury <mizunoyukino@hotmail.com>
2012-05-28 12:27:03 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeBook::Application.config.secret_token = '7126825ab2aa014f7a86878fa5a1c80656d73fac0fc0517b985b4d71beb14d9e46a0d65bce272d23777c5901e361ea6e735db1513fb97fa6aed0c7bbbecc70bc'
