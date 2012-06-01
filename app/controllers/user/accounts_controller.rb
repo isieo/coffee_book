6307c81005017c38653af35d21347ef678ca153f
Gan Yao Aik <yaoaikgan@Gan-Yao-Aiks-MacBook.local>
2012-06-01 18:04:48 +0800

    Added user account profile

--- app/controllers/user/accounts_controller.rb
+++ app/controllers/user/accounts_controller.rb
@@ -1,5 +1,22 @@
 class User::AccountsController < ApplicationController
+  before_filter :find_user, :only => [:index, :edit, :update]
   def index
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(params[:user])
+      flash[:notice] = "User profile updated successfully."
+      redirect_to user_accounts_url
+    else
+      render :action => :edit
+    end
+  end
+  
+  protected
+  def find_user
     @user = current_user
   end
 end
