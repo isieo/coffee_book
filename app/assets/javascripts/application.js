661d3cfdb6deb75bf0d782aa6a17dbcca308bf2d
Gan Yao Aik <yaoaikgan@Gan-Yao-Aiks-MacBook.local>
2012-06-12 16:16:13 +0800

    Flash Message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,12 @@
 //= require twitter/bootstrap
 //= require_tree .
 
+$(document).ready(function() {
+	if ($(".flash").text().length > 0) {
+		$(".flash").slideDown().delay(4000).slideUp(1000);
+	}
+});
+
 $(function() {
   $( ".datepickers" ).datepicker({ dateFormat: 'yy-mm-dd' });
 });
