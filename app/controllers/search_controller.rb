0b04041f91f3d2c94f5798fe449fc06a04dbeca1
SailorMercury <mizunoyukino@hotmail.com>
2012-06-13 16:15:49 +0800

    search function completed for job and user

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,18 @@
 class SearchController < ApplicationController
   
   def search
-    @search = Job.search(params['search'])
+    @search_job = Job.search(params['job'])
+    if @search_job.count == 0
+      @search_user = User.search(params['user'])
+    end
+    
+
     respond_to do |format|
       format.html
       format.json { render json: @search }
     end
+    
+    
   end
   
 end
