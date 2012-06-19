96e3cdb67ff5d8a5b8fb1cb2422c5dc171096147
Gan Yao Aik <yaoaikgan@Gan-Yao-Aiks-MacBook.local>
2012-06-19 16:59:13 +0800

    Add in dragonfly capistrano setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,3 +54,11 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
+
+namespace :dragonfly do
+  desc "Symlink the Rack::Cache files"
+  task :symlink, :roles => [:app] do
+    run "mkdir -p #{shared_path}/tmp/dragonfly && ln -nfs #{shared_path}/tmp/dragonfly #{release_path}/tmp/dragonfly"
+  end
+end
+after 'deploy:update_code', 'dragonfly:symlink'
