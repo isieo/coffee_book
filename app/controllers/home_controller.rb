cd9d1511b9c8e0737b435175a16df938795fae9f
mercury <mercury@mercury-compaq.(none)>
2013-02-19 16:45:10 +0800

    fix coordinates is nil bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,28 +4,24 @@ class HomeController < ApplicationController
   def show
     @jobs = Job.all
     @marker_array = []
-    i = 2
-    if !current_user.nil?
-      if !current_user.address.nil?
-        @lon = current_user.coordinates_longitude
-        @lat = current_user.coordinates_latitude
-        @user = current_user
-        for job in Job.near(@user.address)
-          @marker_array << [job.id, job.coordinates_latitude, job.coordinates_longitude, (self.class.helpers.link_to job.title, job_url(job)),('%.2f' % job.salary),job.date]
-          i = i + 1
-        end
-      end
+    if !params[:lon].nil?
+      @lon = params[:lon]
+      @lat = params[:lat]
+    elsif request.location.longitude > 0
+      @lon = request.location.longitude
+      @lat = request.location.latitude
     else
-      @lon = params[:lon] || (request.location.longitude > 0 ? request.location.longitude : 101.989)
-      @lat = params[:lat] || (request.location.latitude > 0 ? request.location.latitude : 3.129)
-
-      @jobs = Job.near([@lat,@lon],20).each do |job|
-        if !job.coordinates_longitude.nil? && !job.coordinates_latitude.nil?
-          @marker_array << [job.id, job.coordinates_latitude, job.coordinates_longitude, (self.class.helpers.link_to job.title, job_url(job)),('%.2f' % job.salary),job.date] 
-        end
-        i = i + 1
-      end
-
+      @lon = 101.989
+      @lat = 3.129
+    end
+    
+    @lon = current_user.coordinates_longitude if !current_user.address.nil?
+    @lat = current_user.coordinates_latitude if !current_user.address.nil?
+    
+    i = 2
+    for job in Job.near([@lat,@lon],20)
+      @marker_array << [job.id, job.coordinates_latitude, job.coordinates_longitude, (self.class.helpers.link_to job.title, job_url(job)),('%.2f' % job.salary),job.date]
+      i = i + 1
     end
     
     respond_to do |format|
